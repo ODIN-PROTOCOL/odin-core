ac89e9e1a806ba3171584169c6f9468a79f03620
Skriabin <artem.skriabin@distributedlab.com>
2021-02-12 12:38:48 +0200

    Fix tests and ci build

--- yoda/verify_test.go
+++ yoda/verify_test.go
@@ -12,8 +12,8 @@ import (
 
 func TestGetSignBytesVerificationMessage(t *testing.T) {
 	band.SetBech32AddressPrefixesAndBip44CoinType(sdk.GetConfig())
-	validator, _ := sdk.ValAddressFromBech32("bandvaloper1p40yh3zkmhcv0ecqp3mcazy83sa57rgjde6wec")
+	validator, _ := sdk.ValAddressFromBech32("odinvaloper17rprjgtj0krfw3wyl9creueej6ca9dc4a65n80")
 	vmsg := NewVerificationMessage("bandchain", validator, types.RequestID(1), types.ExternalID(1))
-	expected := []byte(`{"chain_id":"bandchain","external_id":1,"request_id":1,"validator":"bandvaloper1p40yh3zkmhcv0ecqp3mcazy83sa57rgjde6wec"}`)
+	expected := []byte(`{"chain_id":"bandchain","external_id":1,"request_id":1,"validator":"odinvaloper17rprjgtj0krfw3wyl9creueej6ca9dc4a65n80"}`)
 	require.Equal(t, expected, vmsg.GetSignBytes())
 }
