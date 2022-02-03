792343aa10bb66dc69dd82cf9185e9fb643904f0
slandymani <slandyman9@gmail.com>
2022-02-03 16:47:10 +0200

    add rest endpoint

--- x/telemetry/types/keys.go
+++ x/telemetry/types/keys.go
@@ -10,14 +10,15 @@ const (
 	RouterKey    = ModuleName
 	QuerierRoute = ModuleName
 
-	QueryTopBalances        = "top_balances"
-	QueryExtendedValidators = "extended_validators"
-	QueryAvgBlockSize       = "avg_block_size"
-	QueryAvgBlockTime       = "avg_block_time"
-	QueryAvgTxFee           = "avg_tx_fee"
-	QueryTxVolume           = "tx_volume"
-	QueryValidatorBlocks    = "validator_blocks"
-	QueryTopValidators      = "top_validators"
+	QueryTopBalances            = "top_balances"
+	QueryExtendedValidators     = "extended_validators"
+	QueryAvgBlockSize           = "avg_block_size"
+	QueryAvgBlockTime           = "avg_block_time"
+	QueryAvgTxFee               = "avg_tx_fee"
+	QueryTxVolume               = "tx_volume"
+	QueryValidatorBlocks        = "validator_blocks"
+	QueryTopValidators          = "top_validators"
+	QueryValidatorByConsAddress = "validator_by_cons_addr"
 
 	DenomTag  = "denom"
 	StatusTag = "status"
