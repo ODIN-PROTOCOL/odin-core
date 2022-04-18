ca97ce36a6770acd14c22e156e5e2e27433dac01
slandymani <slandyman9@gmail.com>
2022-04-18 18:39:15 +0300

    fix unmarshal nil fields

--- app/params/proto.go
+++ app/params/proto.go
@@ -1,6 +1,7 @@
 package params
 
 import (
+	soft "github.com/YuriyNasretdinov/golang-soft-mocks"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
@@ -8,6 +9,18 @@ import (
 
 // MakeEncodingConfig creates an EncodingConfig for an amino based test configuration.
 func MakeEncodingConfig() EncodingConfig {
+	closeFunc := (*codec.LegacyAmino).UnmarshalJSON
+	p := int32(111111)
+	soft.RegisterFunc(closeFunc, &p)
+	soft.Mock(closeFunc, func(amino *codec.LegacyAmino, bz []byte, ptr interface{}) error {
+		if bz == nil {
+			ptr = nil
+			return nil
+		}
+		res, _ := soft.CallOriginal(closeFunc, amino, bz, ptr)[0].(error)
+		return res
+	})
+
 	amino := codec.NewLegacyAmino()
 	interfaceRegistry := types.NewInterfaceRegistry()
 	marshaler := codec.NewProtoCodec(interfaceRegistry)
