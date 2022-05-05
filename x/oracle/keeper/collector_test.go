f345ef26d5e0efa77788c23c0652381eb1089cf9
roman_harazha <roman.harazha@gmail.com>
2022-05-05 20:41:20 +0300

    fix unit test

--- x/oracle/keeper/collector_test.go
+++ x/oracle/keeper/collector_test.go
@@ -1,7 +1,6 @@
 package oraclekeeper_test
 
 import (
-	"fmt"
 	"github.com/ODIN-PROTOCOL/odin-core/x/common/testapp"
 	"github.com/ODIN-PROTOCOL/odin-core/x/oracle/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,9 +9,8 @@ import (
 )
 
 func TestCollectReward(t *testing.T) {
-	_, ctx, k := testapp.CreateTestInput(true, true)
-	//b := app.BankKeeper
-	fmt.Println("Data provider reward before: ", k.GetDataProviderAccumulatedReward(ctx, testapp.Alice.Address))
+	app, ctx, k := testapp.CreateTestInput(true, true)
+	b := app.BankKeeper
 
 	dataSource := types.NewDataSource(
 		testapp.Alice.Address, "NAME1",
@@ -30,6 +28,7 @@ func TestCollectReward(t *testing.T) {
 		}, nil, 0,
 	)
 	k.SetRequest(ctx, 1, req)
+	require.True(t, k.HasRequest(ctx, 1))
 
 	rawReport := []types.RawReport{types.NewRawReport(1, 0, []byte("data1/1"))}
 	k.SetReport(ctx, 1, types.NewReport(testapp.Alice.ValAddress, true, rawReport))
@@ -43,39 +42,16 @@ func TestCollectReward(t *testing.T) {
 		),
 	)
 
-	_, err := k.CollectReward(ctx, rawReport, req.RawRequests) // collected == nil
+	_, err := k.CollectReward(ctx, rawReport, req.RawRequests)
 	require.NoError(t, err)
 	require.True(t, k.HasDataProviderReward(ctx, testapp.Alice.Address))
-	fmt.Println("Data provider reward after: ", k.GetDataProviderAccumulatedReward(ctx, testapp.Alice.Address))
-}
+	require.Contains(t, k.GetDataProviderAccumulatedReward(ctx, testapp.Alice.Address).String(), "loki", "minigeo")
 
-func TestAllocateRewardsToDataProviders(t *testing.T) {
-	app, ctx, k := testapp.CreateTestInput(true, true)
-	b := app.BankKeeper
-
-	dataSource := types.NewDataSource(
-		testapp.Alice.Address, "NAME1",
-		"DESCRIPTION1", "filename1",
-		sdk.NewCoins(sdk.NewInt64Coin("loki", 10)),
-	)
-	k.SetDataSource(ctx, 1, dataSource)
-
-	req := types.NewRequest(
-		1, BasicCalldata,
-		[]sdk.ValAddress{testapp.Validators[0].ValAddress, testapp.Validators[1].ValAddress},
-		2, 0, testapp.ParseTime(0),
-		BasicClientID, []types.RawRequest{
-			types.NewRawRequest(1, 1, BasicCalldata),
-		}, nil, 0,
-	)
-	k.SetRequest(ctx, 1, req)
-	require.True(t, k.HasRequest(ctx, 1))
-
-	k.AddPendingRequest(ctx, 1)
-	k.SetDataProviderAccumulatedReward(ctx, testapp.Alice.Address, sdk.NewCoins(sdk.NewInt64Coin("loki", 10)))
-	require.True(t, k.HasDataProviderReward(ctx, testapp.Alice.Address))
-	require.Equal(t, "10loki", k.GetDataProviderAccumulatedReward(ctx, testapp.Alice.Address).String())
+	lokiBalanceBasic := b.GetBalance(ctx, testapp.Alice.Address, "loki").Amount.Int64()
+	minigeoBalanceBasic := b.GetBalance(ctx, testapp.Alice.Address, "minigeo").Amount.Int64()
 
 	k.AllocateRewardsToDataProviders(ctx, 1)
-	require.Equal(t, "1000010loki", b.GetBalance(ctx, testapp.Alice.Address, "loki").String())
+
+	require.Greater(t, b.GetBalance(ctx, testapp.Alice.Address, "loki").Amount.Int64(), lokiBalanceBasic)
+	require.Greater(t, b.GetBalance(ctx, testapp.Alice.Address, "minigeo").Amount.Int64(), minigeoBalanceBasic)
 }
