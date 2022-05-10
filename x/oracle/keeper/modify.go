abff36d4b6ce3f865949c7ec1e3fd306ea71bcda
roman_harazha <roman.harazha@gmail.com>
2022-05-10 15:06:14 +0300

    fix ability to change data source fee

--- x/oracle/keeper/modify.go
+++ x/oracle/keeper/modify.go
@@ -2,11 +2,19 @@ package oraclekeeper
 
 import (
 	"github.com/ODIN-PROTOCOL/odin-core/x/oracle/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // modify returns new value if it is not `DoNotModify`. Returns old value otherwise
 func modify(oldVal string, newVal string) string {
 	if newVal == types.DoNotModify {
+		return oldVal
+	}
+	return newVal
+}
+
+func modifyFee(oldVal, newVal sdk.Coins) sdk.Coins {
+	if newVal == nil {
 		return oldVal
 	}
 	return newVal
