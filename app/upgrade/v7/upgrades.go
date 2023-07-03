8cb636a08298404ec433c48c1471cdd9de844941
monopauli <steamleopac@gmail.com>
2023-07-03 17:58:57 +0200

    updated target addresses

--- app/upgrade/v7/upgrades.go
+++ app/upgrade/v7/upgrades.go
@@ -78,34 +78,52 @@ func undelegate(
 
 }
 
-func getAddresses() []sdk.AccAddress {
-	var addresses [15]string
+func getAddresses() [][]sdk.AccAddress {
+	var addresses [15][15]string
 
-	addresses[0] = "odin13jgurnd72ftvy79tjtl3s8e6xvsuupwreft506"  //continuous vesting account
-	addresses[1] = "odin1lqk7gsq40dskpcukavkwuh7t73cnh9tjfgqxkp"  //base account
-	addresses[2] = "odin1qerev5feaft35fp2n3ept7fdsheud52z0gzne0"  //base account
-	addresses[3] = "odin14p9vgtynfy394hmz0tcrrs78e4whj5z6kmp09v"  //continuous vesting account
-	addresses[4] = "odin122qmr2s3583msah5fk5jwc7557v30kn0pj50sd"  //continuous vesting account
-	addresses[5] = "odin1vmfekljy9haqgyzsfvyn85xa8xdd8wcum33cf4"  //base account
-	addresses[6] = "odin1vu0kf5ztrscm9fvc0gg3nqdfj7rfr6sc2u9r0v"  //continuous vesting account
-	addresses[7] = "odin1r532h9h54eaylxg49ll6vlr8epcl687s2x9ln7"  //continuous vesting account
-	addresses[8] = "odin1nvzkd37yqhw9pn9eljf3mvrneug3hr0r3xyc62"  //continuous vesting account
-	addresses[9] = "odin16hxrt4scaly02caaskhe0984rzl0fj490c853y"  //continuous vesting account
-	addresses[10] = "odin1n2apxttn8f3uzrrzrpgkrxascwdhcwh3uyejxe" //continuous vesting account
-	addresses[11] = "odin1sm56gxdlwd32dzcps6uzprudvawuqg6mv83eq9" //continuous vesting account
-	addresses[12] = "odin1n8gwpl4s75qhhvtlmlp5y5946acjnlutv4krud" //continuous vesting account
-	addresses[13] = "odin1qe8v4nx9l06x4m3cy3wk7al6h4ww7js5rgpgp0" //continuous vesting account
-	addresses[14] = "odin1vu0kf5ztrscm9fvc0gg3nqdfj7rfr6sc2u9r0v" //continuous vesting account
+	addresses[0][0] = "odin13jgurnd72ftvy79tjtl3s8e6xvsuupwreft506"  //continuous vesting account
+	addresses[0][1] = "odin18ffxuvq37jj6dwrsrd3qs0dy2s2tg3htz6h9ed"  //new account
+	addresses[1][0] = "odin1lqk7gsq40dskpcukavkwuh7t73cnh9tjfgqxkp"  //base account
+	addresses[1][1] = "odin1sz7e7e90v7jsph93h46wz07sevvun2lwhcu4p0"  //new account
+	addresses[2][0] = "odin1qerev5feaft35fp2n3ept7fdsheud52z0gzne0"  //base account
+	addresses[2][1] = "odin1n27lkvt7t5cadsqn2320tpc0hsa2v8gxa35qr6"  //new account
+	addresses[3][0] = "odin14p9vgtynfy394hmz0tcrrs78e4whj5z6kmp09v"  //continuous vesting account
+	addresses[3][1] = "odin1wf2uw3jd2cj7k73qyd2hz6sqgmkxvvm2heygac"  //new account
+	addresses[4][0] = "odin122qmr2s3583msah5fk5jwc7557v30kn0pj50sd"  //continuous vesting account
+	addresses[4][1] = "odin17x2h8z50r5qkzlkqd56j6kwhun7ef0jy7kyktf"  //new account
+	addresses[5][0] = "odin1vmfekljy9haqgyzsfvyn85xa8xdd8wcum33cf4"  //base account
+	addresses[5][1] = "odin1jrtgrk3u808f7snxm63n5s8nqr6c2prfa3ttn5"  //new account
+	addresses[6][0] = "odin1vu0kf5ztrscm9fvc0gg3nqdfj7rfr6sc2u9r0v"  //continuous vesting account
+	addresses[6][1] = "odin1mc0tkfnkfjpal62fkpfn2g0j5s3j69vat4dw4p"  //new account
+	addresses[7][0] = "odin1r532h9h54eaylxg49ll6vlr8epcl687s2x9ln7"  //continuous vesting account
+	addresses[7][1] = "odin1npu6zn9mrztwkq2pr27vd3zzq485hqcg9umna8"  //new account
+	addresses[8][0] = "odin1nvzkd37yqhw9pn9eljf3mvrneug3hr0r3xyc62"  //continuous vesting account
+	addresses[8][1] = "odin1ye36q77r23mnlps96wr5ckgy83x5emhazx7m3f"  //new account
+	addresses[9][0] = "odin16hxrt4scaly02caaskhe0984rzl0fj490c853y"  //continuous vesting account
+	addresses[9][1] = "odin19mse0ejcpqc2wtyqgyj3rnc6qn54fn987g2pq9"  //new account
+	addresses[10][0] = "odin1n2apxttn8f3uzrrzrpgkrxascwdhcwh3uyejxe" //continuous vesting account
+	addresses[10][1] = "odin1hqpcmxrrfyqz7r895vgjl32u9yvy4vzezupk2s" //new account
+	addresses[11][0] = "odin1sm56gxdlwd32dzcps6uzprudvawuqg6mv83eq9" //continuous vesting account
+	addresses[11][1] = "odin1mt98jffmfaa3g25yp6q00l7lk6xrfqshsjjkdv" //new account
+	addresses[12][0] = "odin1n8gwpl4s75qhhvtlmlp5y5946acjnlutv4krud" //continuous vesting account
+	addresses[12][1] = "odin18925q6zxy7s7x7jcuru2zavypp5ll7acs2g923" //new account
+	addresses[13][0] = "odin1qe8v4nx9l06x4m3cy3wk7al6h4ww7js5rgpgp0" //continuous vesting account
+	addresses[13][1] = "odin14mhwrtxluyzlvy98gpcld2m9qtk6qahgx3ncnj" //new account
+	addresses[14][0] = "odin1vu0kf5ztrscm9fvc0gg3nqdfj7rfr6sc2u9r0v" //continuous vesting account
+	addresses[14][1] = "odin1c43q5hyrs5jsu303z8h44wv36f7l0fgx46m402" //new account
 
-	var accAddresses []sdk.AccAddress
+	var accAddresses [][]sdk.AccAddress
 
-	for _, addr := range addresses {
-		accAddr, err := sdk.AccAddressFromBech32(addr)
-		if err != nil {
-			panic(fmt.Sprintf("account address is not valid bech32: %s", accAddr))
+	for _, addrs := range addresses {
+		var accaddrs []sdk.AccAddress
+		for _, addr := range addrs {
+			accAddr, err := sdk.AccAddressFromBech32(addr)
+			if err != nil {
+				panic(fmt.Sprintf("account address is not valid bech32: %s", accAddr))
+			}
+			accaddrs = append(accaddrs, accAddr)
 		}
-		accAddresses = append(accAddresses, accAddr)
-
+		accAddresses = append(accAddresses, accaddrs)
 	}
 	return accAddresses
 }
@@ -196,14 +214,11 @@ func CreateUpgradeHandler(
 		// ctx.Logger().Info("getting all account balances")
 		// totalCoins := sumBalances(ctx, stakingkeeper, accountkeeper, bankkeeper, addresses)
 
-		//Getting account address of new address
-		newAddr := addrToAccAddr(newAddress)
-
 		ctx.Logger().Info("sending coins from addresses to new address")
-		for _, address := range addresses {
-			balance, err := getBalance(ctx, stakingkeeper, accountkeeper, bankkeeper, address)
+		for _, addrs := range addresses {
+			balance, err := getBalance(ctx, stakingkeeper, accountkeeper, bankkeeper, addrs[0])
 			if err != nil {
-				sendCoins(ctx, bankkeeper, address, newAddr, balance)
+				sendCoins(ctx, bankkeeper, addrs[0], addrs[1], balance)
 			}
 
 		}
