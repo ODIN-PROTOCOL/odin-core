0d7077ebc431b0456e1202a355a4a6ef56fea2f8
monopauli <steamleopac@gmail.com>
2023-07-13 13:03:47 +0200

    fixed buggy error handling

--- app/upgrade/v7/upgrades.go
+++ app/upgrade/v7/upgrades.go
@@ -12,7 +12,6 @@ import (
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
-	"github.com/ODIN-PROTOCOL/odin-core/x/bank"
 	mintkeeper "github.com/ODIN-PROTOCOL/odin-core/x/mint/keeper"
 )
 
@@ -217,12 +216,12 @@ func CreateUpgradeHandler(
 		for _, addrs := range addresses {
 			balance, err := getBalance(ctx, stakingkeeper, accountkeeper, bankkeeper, addrs[0])
 			if err != nil {
-				sendCoins(ctx, bankkeeper, addrs[0], addrs[1], balance)
+				panic(fmt.Sprintf("Could not send coins"))
 			}
+			sendCoins(ctx, bankkeeper, addrs[0], addrs[1], balance)
 
 		}
 
-		vm["bank"] = bank.AppModule{}.ConsensusVersion()
 		newVM, err := mm.RunMigrations(ctx, configurator, vm)
 		if err != nil {
 			return nil, err
