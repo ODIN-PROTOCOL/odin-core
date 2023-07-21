d2b4b5fd6fcf7c540f772258d52e7f788d4cc715
monopauli <steamleopac@gmail.com>
2023-07-21 13:46:04 +0200

    updated addresses

--- app/upgrade/v7/upgrades.go
+++ app/upgrade/v7/upgrades.go
@@ -81,34 +81,20 @@ func getAddresses() [][]sdk.AccAddress {
 	var addresses [][]string
 
 	addresses = append(addresses, []string{"odin13jgurnd72ftvy79tjtl3s8e6xvsuupwreft506", "odin18u8kaxjn3pdums92exr0j6pjmt77ne7c2tkwth"})
-	// addresses[1][0] = "odin1lqk7gsq40dskpcukavkwuh7t73cnh9tjfgqxkp"  //base account
-	// addresses[1][1] = "odin1sz7e7e90v7jsph93h46wz07sevvun2lwhcu4p0"  //new account
-	// addresses[2][0] = "odin1qerev5feaft35fp2n3ept7fdsheud52z0gzne0"  //base account
-	// addresses[2][1] = "odin1n27lkvt7t5cadsqn2320tpc0hsa2v8gxa35qr6"  //new account
-	// addresses[3][0] = "odin14p9vgtynfy394hmz0tcrrs78e4whj5z6kmp09v"  //continuous vesting account
-	// addresses[3][1] = "odin1wf2uw3jd2cj7k73qyd2hz6sqgmkxvvm2heygac"  //new account
-	// addresses[4][0] = "odin122qmr2s3583msah5fk5jwc7557v30kn0pj50sd"  //continuous vesting account
-	// addresses[4][1] = "odin17x2h8z50r5qkzlkqd56j6kwhun7ef0jy7kyktf"  //new account
-	// addresses[5][0] = "odin1vmfekljy9haqgyzsfvyn85xa8xdd8wcum33cf4"  //base account
-	// addresses[5][1] = "odin1jrtgrk3u808f7snxm63n5s8nqr6c2prfa3ttn5"  //new account
-	// addresses[6][0] = "odin1vu0kf5ztrscm9fvc0gg3nqdfj7rfr6sc2u9r0v"  //continuous vesting account
-	// addresses[6][1] = "odin1mc0tkfnkfjpal62fkpfn2g0j5s3j69vat4dw4p"  //new account
-	// addresses[7][0] = "odin1r532h9h54eaylxg49ll6vlr8epcl687s2x9ln7"  //continuous vesting account
-	// addresses[7][1] = "odin1npu6zn9mrztwkq2pr27vd3zzq485hqcg9umna8"  //new account
-	// addresses[8][0] = "odin1nvzkd37yqhw9pn9eljf3mvrneug3hr0r3xyc62"  //continuous vesting account
-	// addresses[8][1] = "odin1ye36q77r23mnlps96wr5ckgy83x5emhazx7m3f"  //new account
-	// addresses[9][0] = "odin16hxrt4scaly02caaskhe0984rzl0fj490c853y"  //continuous vesting account
-	// addresses[9][1] = "odin19mse0ejcpqc2wtyqgyj3rnc6qn54fn987g2pq9"  //new account
-	// addresses[10][0] = "odin1n2apxttn8f3uzrrzrpgkrxascwdhcwh3uyejxe" //continuous vesting account
-	// addresses[10][1] = "odin1hqpcmxrrfyqz7r895vgjl32u9yvy4vzezupk2s" //new account
-	// addresses[11][0] = "odin1sm56gxdlwd32dzcps6uzprudvawuqg6mv83eq9" //continuous vesting account
-	// addresses[11][1] = "odin1mt98jffmfaa3g25yp6q00l7lk6xrfqshsjjkdv" //new account
-	// addresses[12][0] = "odin1n8gwpl4s75qhhvtlmlp5y5946acjnlutv4krud" //continuous vesting account
-	// addresses[12][1] = "odin18925q6zxy7s7x7jcuru2zavypp5ll7acs2g923" //new account
-	// addresses[13][0] = "odin1qe8v4nx9l06x4m3cy3wk7al6h4ww7js5rgpgp0" //continuous vesting account
-	// addresses[13][1] = "odin14mhwrtxluyzlvy98gpcld2m9qtk6qahgx3ncnj" //new account
-	// addresses[14][0] = "odin1vu0kf5ztrscm9fvc0gg3nqdfj7rfr6sc2u9r0v" //continuous vesting account
-	// addresses[14][1] = "odin1c43q5hyrs5jsu303z8h44wv36f7l0fgx46m402" //new account
+	addresses = append(addresses, []string{"odin1lqk7gsq40dskpcukavkwuh7t73cnh9tjfgqxkp", "odin1sz7e7e90v7jsph93h46wz07sevvun2lwhcu4p0"})
+	addresses = append(addresses, []string{"odin1qerev5feaft35fp2n3ept7fdsheud52z0gzne0", "odin1n27lkvt7t5cadsqn2320tpc0hsa2v8gxa35qr6"})
+	addresses = append(addresses, []string{"odin14p9vgtynfy394hmz0tcrrs78e4whj5z6kmp09v", "odin1wf2uw3jd2cj7k73qyd2hz6sqgmkxvvm2heygac"})
+	addresses = append(addresses, []string{"odin122qmr2s3583msah5fk5jwc7557v30kn0pj50sd", "odin17x2h8z50r5qkzlkqd56j6kwhun7ef0jy7kyktf"})
+	addresses = append(addresses, []string{"odin1vmfekljy9haqgyzsfvyn85xa8xdd8wcum33cf4", "odin1jrtgrk3u808f7snxm63n5s8nqr6c2prfa3ttn5"})
+	addresses = append(addresses, []string{"odin1vu0kf5ztrscm9fvc0gg3nqdfj7rfr6sc2u9r0v", "odin1mc0tkfnkfjpal62fkpfn2g0j5s3j69vat4dw4p"})
+	addresses = append(addresses, []string{"odin1r532h9h54eaylxg49ll6vlr8epcl687s2x9ln7", "odin1npu6zn9mrztwkq2pr27vd3zzq485hqcg9umna8"})
+	addresses = append(addresses, []string{"odin1nvzkd37yqhw9pn9eljf3mvrneug3hr0r3xyc62", "odin1ye36q77r23mnlps96wr5ckgy83x5emhazx7m3f"})
+	addresses = append(addresses, []string{"odin16hxrt4scaly02caaskhe0984rzl0fj490c853y", "odin19mse0ejcpqc2wtyqgyj3rnc6qn54fn987g2pq9"})
+	addresses = append(addresses, []string{"odin1n2apxttn8f3uzrrzrpgkrxascwdhcwh3uyejxe", "odin1hqpcmxrrfyqz7r895vgjl32u9yvy4vzezupk2s"})
+	addresses = append(addresses, []string{"odin1sm56gxdlwd32dzcps6uzprudvawuqg6mv83eq9", "odin1mt98jffmfaa3g25yp6q00l7lk6xrfqshsjjkdv"})
+	addresses = append(addresses, []string{"odin1n8gwpl4s75qhhvtlmlp5y5946acjnlutv4krud", "odin18925q6zxy7s7x7jcuru2zavypp5ll7acs2g923"})
+	addresses = append(addresses, []string{"odin1qe8v4nx9l06x4m3cy3wk7al6h4ww7js5rgpgp0", "odin14mhwrtxluyzlvy98gpcld2m9qtk6qahgx3ncnj"})
+	addresses = append(addresses, []string{"odin1vu0kf5ztrscm9fvc0gg3nqdfj7rfr6sc2u9r0v", "odin1c43q5hyrs5jsu303z8h44wv36f7l0fgx46m402"})
 
 	var accAddresses [][]sdk.AccAddress
 
@@ -219,7 +205,6 @@ func CreateUpgradeHandler(
 				panic(fmt.Sprintf("Could not send coins"))
 			}
 			sendCoins(ctx, bankkeeper, addrs[0], addrs[1], balance)
-
 		}
 
 		newVM, err := mm.RunMigrations(ctx, configurator, vm)
