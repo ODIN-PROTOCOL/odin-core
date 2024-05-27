1b130287a11e84515cc7a57c17627cf427fa7775
slandymani <slandyman9@gmail.com>
2024-05-27 12:17:58 +0300

    remove debug logs

--- x/mint/abci.go
+++ x/mint/abci.go
@@ -14,52 +14,43 @@ import (
 func BeginBlocker(ctx context.Context, k keeper.Keeper, ic minttypes.InflationCalculationFn) error {
 	defer telemetry.ModuleMeasureSince(minttypes.ModuleName, time.Now(), telemetry.MetricKeyBeginBlocker)
 
-	goCtx := sdk.UnwrapSDKContext(ctx)
-	goCtx.Logger().Info("here1")
 	// fetch stored minter & params
 	minter, err := k.GetMinter(ctx)
 	if err != nil {
 		return err
 	}
 
-	goCtx.Logger().Info("here2")
 	params, err := k.GetParams(ctx)
 	if err != nil {
 		return err
 	}
 
-	goCtx.Logger().Info("here3")
 	// recalculate inflation rate
 	totalStakingSupply, err := k.StakingTokenSupply(ctx)
 	if err != nil {
 		return err
 	}
 
-	goCtx.Logger().Info("here4")
 	bondedRatio, err := k.BondedRatio(ctx)
 	if err != nil {
 		return err
 	}
 
-	goCtx.Logger().Info("here5")
 	minter.Inflation = ic(ctx, minter, params, bondedRatio)
 	minter.AnnualProvisions = minter.NextAnnualProvisions(params, totalStakingSupply)
 	if err = k.SetMinter(ctx, minter); err != nil {
 		return err
 	}
 
-	goCtx.Logger().Info("here6")
 	// mint coins, update supply
 	mintedCoin := minter.BlockProvision(params)
 	mintedCoins := sdk.NewCoins(mintedCoin)
 
-	goCtx.Logger().Info("here7")
 	err = k.MintCoins(ctx, mintedCoins)
 	if err != nil {
 		panic(err)
 	}
 
-	goCtx.Logger().Info("here8")
 	// send the minted coins to the fee collector account
 	err = k.AddCollectedFees(ctx, mintedCoins)
 	if err != nil {
